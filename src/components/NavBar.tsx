27c8d44ca7c4c560339a03f7f23027c2bbb8a74a
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-21 17:10:37 +0530

    navigate added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,16 @@
+import {useNavigate} from "react-router-dom"
+
+
 const NavBar = () => {
+  const navigate=useNavigate()
   
+  const pageChange = (e:any) => {
+    navigate(e.target.title) 
+  }
   return (
     <div className='flex justify-between p-4 px-10 bg-blue-200 shadow-sm sticky top-0 z-10'>
-      <div className="hover:cursor-pointer">Home</div>
-      <div className="hover:cursor-pointer">Login</div>
+      <div className="hover:cursor-pointer" onClick={pageChange} title="/">Home</div>
+      <div className="hover:cursor-pointer"  onClick={pageChange}  title="/login">Login</div>
     </div>
   )
 }
