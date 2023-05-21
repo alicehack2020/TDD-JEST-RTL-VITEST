428969c94b97f2b1dd7ddb6212b190904e0bb220
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-21 16:12:28 +0530

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Login from './components/auth/Login'
 function App() {
   return (
     <>
-      <div className='grid text-left place-content-center items-center h-screen'>
+      <div>
         <div>
           <Login/>
         </div>
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -4,23 +4,31 @@ const Login = () => {
   const [userName,setUserName]=useState("") 
   const [userPassword,setUserPassword]=useState("") 
 
+
+  const haddleLogin = () => {
+    console.log("clicked")
+  }
+
   return (
-    <div>
+    <div className='grid text-left justify-center items-center h-screen shadow-xl'>
+    <div >
       <h1
         data-testid="welcome"
-        className='text-xl text-blue-400 py-4'>
-        Welcome to tree foundation
+        className='text-xl text-blue-400 '>
+        Welcome To Tree Foundation
       </h1> 
       <div>
         <label data-testid="user name label" className="mt-4">Enter Your Name</label><br />
-        <input type="text" name="" id="" placeholder="Enter Your Name" className="mb-2 mt-2" value={userName} onChange={e=>setUserName(e.target.value)}/><br />
+        <input type="text" name="userName" placeholder="Enter Your Name" className="w-full my-2 p-2 border-2 border-gray-300 rounded-md outline-transparent hover:outline-blue-400" value={userName} onChange={e=>setUserName(e.target.value)}/><br />
         
         <label data-testid="user password label" >Enter Your Name</label><br />
-        <input type="password" name="" id="" placeholder="Enter Your Password" className="mt-2" value={userPassword} onChange={e=>setUserPassword(e.target.value)}/>
-        
-        <button data-testid="loginButton" disabled={!userName || !userPassword}></button>
+        <input type="password" name="userPassword"   placeholder="Enter Your Password" className="w-full my-2 p-2 border-2 border-gray-300 rounded-md outline-transparent hover:outline-blue-400" value={userPassword} onChange={e=>setUserPassword(e.target.value)}/><br />
+        <button data-testid="loginButton" disabled={!userName || !userPassword} onClick={haddleLogin} className="w-full p-2 bg-blue-300 text-xl hover:cursor-pointer">Login</button>
+       
       </div>
-    </div>
+      
+      </div>
+      </div>
   )
 }
 
